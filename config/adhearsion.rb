783b6375978776182133e9afc074fb25733cf071
Todd Gould <tgould@five9.com>
2013-08-30 13:18:36 -0700

    added process name: ramon to config

--- config/adhearsion.rb
+++ config/adhearsion.rb
@@ -35,6 +35,7 @@ Adhearsion.config do |config|
   config.punchblock.password = "r@m0n" # Your AMI password
   config.punchblock.host = "127.0.0.1" # Your AMI host
 
+  config.platform.process_name = "ramon"
   ##
   # Use with FreeSWITCH
   #
